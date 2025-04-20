3d803c9f0d126abc4f315131ce70cc7e48c821bc
sanaa <sanaa.chraa@hotmail.com>
2025-04-20 19:36:31 +0200

    bestand tijdelijk.py toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -3,25 +3,20 @@ prijzen = {
     "vanille": 4,
     "chocolade": 5 
 } 
-aanbieding = prijzen["vanille"], * 0.8
-reclame_tekst = f"vandaag in de aanbieding: vanille_ijs, 1 liter - slechts {aanbieding}"
+aanbieding = prijzen["aardbei"] * 0.8
+reclame_tekst = f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
 
-reclame_tekst2 = reclame_tekst [: 36]
+reclame_tekst2 = reclame_tekst [:63]
 
 reclame_tekst3 = reclame_tekst2.upper() 
 
-reclame_tekst4 = reclame_tekst3.replace("", "\n")
+reclame_tekst4 = reclame_tekst3.split()
 
-for e1 in reclame_tekst4.split("\n"):
-    e1 = e1.lower()
-
-    if len (e1) > 4:
-        print(e1)
-woorden_lijst = reclame_tekst4.split("/n")
-lange_woord = [woord.lower() for woord in woorden_lijst if len(woord)> 4]
-
-if len(lange_woord) == len([w for w in woorden_lijst if len(w) > 4]):
-    print ("Controle is gelukt.")
-    print("bestand tijdelijke.py\ngecodeerd")
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    else:
+        print(el.lower())    
+        
  
 
