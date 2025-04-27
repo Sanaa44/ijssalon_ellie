1f065a5c22684a2a3fb03c90d13ac2a3ea5e10f9
sanaa <sanaa.chraa@hotmail.com>
2025-04-27 20:15:46 +0200

    bestand reclame.py toegevoegd

--- algemen_functies.py
+++ algemen_functies.py
@@ -1,12 +1,15 @@
 def mijn_functie_1(x):
     return x * x
 
+def mijn_functie_2(a, b):
+    return [a + b, a - b, b * a, a // b ]
+
 print (mijn_functie_1(2))
 print(mijn_functie_1(4))
 print(mijn_functie_1(10))
 print(mijn_functie_1(12))
 
-def mijn_functie_2(lijst):
-    getal1 = lijst[0]
-    getal2 = lijst[1]
-    return [getal1 + getal2, getal1 - getal2, getal2, getal1 / getal2]
+print(mijn_functie_2(12, 3)) 
+print(mijn_functie_2(12, 2))
+print(mijn_functie_2(10, 5))
+print(mijn_functie_2(100, 20))
--- reclame.py
+++ reclame.py
@@ -1,9 +1,6 @@
 
-from algemen_functies import mijn_functie_2 
+from algemen_functies import mijn_functie_1, mijn_functie_2  
 
-def combinatie (invoer_lijst_2):
-    kort_lijst = hoog_en_laag (invoer_lijst_2)
-    return mijn_functie_2 (kort_lijst)
 
 def aabieding_1( smaak, prijs, korting):
     nieuwe_prijs = prijs - (prijs * korting)
@@ -12,25 +9,33 @@ def aabieding_1( smaak, prijs, korting):
 
 def inkomsten_totaal(inkomsten, btw):
     totaal = sum(inkomsten)
-    btw_bedrag = totaal * btw
-    totaal_met_btw = round (totaal_met_btw, 2)
+    btw_bedrag = round (totaal * btw, 2)
     return f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro btw betaald dient te worden"
 
 def hoog_en_laag(mijn_lijst):
     return [max(mijn_lijst), min(mijn_lijst)]
 
-def meervoudig(invoer_lijst):
-    hoogste_en_laagste = hoog_en_laag (invoer_lijst)
-    laatste_waarde = invoer_lijst[-1]
-    return[hoogste_en_laagste[0], laatste_waarde]
-
 def gemiddelde(mijn_lijst):
-    totaal = sum(mijn_lijst)
-    aantal = len(mijn_lijst)
-    gemiddelde_waarde = totaal / aantal
+    gemiddelde_waarde = sum(mijn_lijst) / len(mijn_lijst)
     gemiddelde_waarde = round(gemiddelde_waarde, 2)
-    gemiddelde_str = str(gemiddelde_waarde).replace('.', ',')
-    return f"De inkomsten deze week zijn {gemiddelde_str} euro"
+    return f"De inkomsten deze week zijn {gemiddelde_waarde} euro."
 
+def meervoudig(invoer_lijst):
+    korte_lijst = hoog_en_laag (invoer_lijst)
+    return korte_lijst
+
+def combinatie (mijn_lijst):
+    resultaat = []
+    for i in range (0, len(mijn_lijst)- 1, 2):
+        a = mijn_lijst[i]
+        b = mijn_lijst[i+1]
+        resultaat = mijn_functie_2 (a, b)
+        resultaat.append(resultaat)
+    return resultaat
+
+print (aabieding_1("aardbei", 4, 0.1))
+print(inkomsten_totaal([220, 430, 125, 160, 205, 90, 343], 0.09))
+print(hoog_en_laag([220, 430, 125, 160, 205, 90, 345]))
 print (gemiddelde([220, 430, 125, 160, 205, 90, 345]))
 print (meervoudig([10, 5, 3, 2, 1, 2, 9]))
+print (combinatie([10, 5, 2, 2, 9, 1]))
